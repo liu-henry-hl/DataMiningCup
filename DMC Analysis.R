c22ef0485650605e9e496985b0222aaf74654899
leafa <mxyang42@gmail.com>
2014-03-16 23:56:15 -0400

    sdfsd

--- DMC Analysis.R
+++ DMC Analysis.R
@@ -8,7 +8,7 @@
 #                 in transactions in the given timeframe
 #                 (regardless of whether transactions occur)
 do_offer_coupon <- function(trxn_rate, est_amt_trxn){
-  return (1.0 - exp(-trxn_rate) - exp(-trxn_rate) * trxn_rate/2.0 > 3.0/(0.1 * est_amt_trxn + 8.0))
+  return (1.0 - exp(-trxn_rate) - exp(-trxn_rate) * trxn_rate > 3.0/(0.1 * est_amt_trxn + 8.0))
 }
 
 find_trxn_rate <- function(customer_id){
@@ -16,7 +16,6 @@ find_trxn_rate <- function(customer_id){
 }
 
 # Raw Data
-# setwd("C:\\Users\\office\\Desktop\\uwdmc2014_files")
 build_auth_m216_raw = read.csv("build_auth_m216.csv",header = T, sep = ",")
 customer_zip_raw = read.csv("customer_zip.csv",header = T, sep = ",")
 merchant_metrics_raw = read.csv("merchant_metrics.csv",header = T, sep = ",")
@@ -46,12 +45,9 @@ View(super_ind_names.new)
 merchant_metrics_merge <- merge(merchant_metrics_copy, super_ind_names.new, all.y=T) # Right join (default)
 View(merchant_metrics_merge)
 
-#validation_auth_copy <- validation_auth_raw
-
 #find the average trxn amount of each customer
 customer_id = unique(build_auth_m216_copy$acct_id_code)
 customer_info = data.frame(customer_id, total_trxn_amt=numeric(length(customer_id)), num_of_trxns=numeric(length(customer_id)), amt_per_trxn=numeric(length(customer_ids)))
-#View(customer_info)
 customer_idx = 1
 for (i in 1:nrow(build_auth_m216_copy)) {
   to_be_recorded = build_auth_m216_copy[i, 1:6]
@@ -66,13 +62,10 @@ for (i in 1:nrow(build_auth_m216_copy)) {
       customer_idx = customer_idx + 1
     }
   }
-<<<<<<< HEAD
 }
+customer_info$amt_per_trxn <- customer_info$total_trxn_amt / customer_info$num_of_trxns
+customer_info$trxn_rate <- find_trxn_rate(customer_info$customer_id)
 #View(customer_info)
-=======
-  
-}
 
 #add column specifying whether we want to give each customer a coupon
-customer_info$do_coupon <- ifelse(do_offer_coupon(customer_info$customer_ids), 1, 0))
->>>>>>> e09b0b751861de94e6374cc9e955db071653262d
+customer_info$do_coupon <- ifelse(do_offer_coupon(customer_info$customer_id), 1, 0))
